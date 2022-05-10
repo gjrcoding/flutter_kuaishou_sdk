dbb4886c27e26c0605c56945b32afce966018c95
logan <gengjiarong@gmail.com>
2022-05-10 12:14:12 +0800

    Initial commit

---
+++ ios/Classes/FlutterKuaishouSdkPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterKuaishouSdkPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FlutterKuaishouSdkPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterKuaishouSdkPlugin.h"
+
+@implementation FlutterKuaishouSdkPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_kuaishou_sdk"
+            binaryMessenger:[registrar messenger]];
+  FlutterKuaishouSdkPlugin* instance = [[FlutterKuaishouSdkPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
